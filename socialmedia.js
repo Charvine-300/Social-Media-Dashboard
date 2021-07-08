dd989fc912529f7c2ef1a006e3cea886c099a48c
LordEra <marvinezenwekwe@gmail.com>
2021-07-09 00:34:56 +0100

    Dark Mode Finished

--- socialmedia.js
+++ socialmedia.js
@@ -1,21 +1,91 @@
 function setUp() {
     var button = document.getElementById("circle-switch");
+    var back = document.getElementById("background-holder");
     var buttonBack = document.getElementById("toggle-master");
+    var headBack = document.getElementById("head-wrapper");
+    var spliter = document.getElementById("line-split");
+    var dataHead = document.getElementById("present-overview");
+    var headers = document.querySelectorAll("#head-words");
+    var dashboard = document.getElementById("head-word");
+    var potters = document.querySelectorAll("#potter-word");
+    var dataCarriers = document.querySelectorAll("#data-words");
+    var topper = document.querySelectorAll("#names");
+    var lower = document.querySelectorAll("#usernames");
+    var readers = document.querySelectorAll("#hayley-baby");
+    var mincers = document.querySelectorAll("#social-info");
+    var pincers = document.querySelectorAll("#kept-scores");
 
-    button.setAttribute("class", "baseline");
+
 
     button.onclick = function() {
 
-        if (button.className == "baseline") {
-            button.style.float = "left";
-            button.style.backgroundColor = "hsl(232, 19%, 15%)";
-            buttonBack.style.background = "linear-gradient (90deg, hsl(210, 78%, 56%), hsl(146, 68%, 55%))"; 
+        if (button.className == "") {
+            button.className = "first";
+            buttonBack.className = "second";
+            back.className = "third";
+            headBack.className = "fourth";
+            spliter.className = "fifth";
+            dataHead.className = "eighth";
+            dashboard.className = "ninth";
+            Array.from(headers).forEach(function(header) {
+                header.className = "sixth";
+            });
+            Array.from(dataCarriers).forEach(function(data) {
+                data.className = "seventh";
+            });
+            Array.from(potters).forEach(function(potter) {
+                potter.className = "tenth";
+            });
+            Array.from(topper).forEach(function(top) {
+                top.className = "eleventh";
+            });
+            Array.from(lower).forEach(function(low) {
+                low.className = "twelveth";
+            });
+            Array.from(readers).forEach(function(read) {
+                read.className = "thirteenth";
+            });
+            Array.from(mincers).forEach(function(mince) {
+                mince.className = "fourteenth";
+            });
+            Array.from(pincers).forEach(function(pince) {
+                pince.className = "fifteenth";
+            });
         }
  
-       if (button.style.float = "left") {
-        button.style.float = "right";
-        button.style.backgroundColor = "hsl(0, 0%, 97%)";
-       }
+       else {
+            back.className = "";
+            button.className = "";
+            spliter.className = "";
+            headBack.className = "";
+            buttonBack.className = "";
+            dataHead.className = "";
+            dashboard.className = "";
+            Array.from(headers).forEach(function(header) {
+                header.className = "";
+            });
+            Array.from(dataCarriers).forEach(function(data) {
+                data.className = "";
+            });
+            Array.from(potters).forEach(function(potter) {
+                potter.className = "";
+            });
+            Array.from(topper).forEach(function(top) {
+                top.className = "";
+            });
+            Array.from(lower).forEach(function(low) {
+                low.className = "";
+            });
+            Array.from(readers).forEach(function(read) {
+                read.className = "";
+            });
+            Array.from(mincers).forEach(function(mince) {
+                mince.className = "";
+            });
+            Array.from(pincers).forEach(function(pince) {
+                pince.className = "";
+            });
+        }
         
         
     }
